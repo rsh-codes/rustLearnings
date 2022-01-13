b13d7359b363137e5ec5238c1cb7169322b4c0e5
_ <ryanscotthutton@gmail.com>
2022-01-12 20:45:06 -0600

    end of chap3

---
+++ chap4/target/debug/chap4.d
@@ -0,0 +1,1 @@
+/Users/zohvek/Documents/GitHub/rustLearnings/chap4/target/debug/chap4: /Users/zohvek/Documents/GitHub/rustLearnings/chap4/src/main.rs
---
+++ chap4/target/debug/deps/chap4-f605242b377dce40.d
@@ -0,0 +1,5 @@
+/Users/zohvek/Documents/GitHub/rustLearnings/chap4/target/debug/deps/chap4-f605242b377dce40: src/main.rs
+
+/Users/zohvek/Documents/GitHub/rustLearnings/chap4/target/debug/deps/chap4-f605242b377dce40.d: src/main.rs
+
+src/main.rs:
